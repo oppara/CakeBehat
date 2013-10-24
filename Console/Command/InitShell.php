9e24d66ec2dadc993bc81cb3617d2879ad166861
oppara <oppara@oppara.tv>
2013-10-24 18:23:05 +0900

    Copy features to app/Test

--- Console/Command/InitShell.php
+++ Console/Command/InitShell.php
@@ -4,11 +4,11 @@ App::uses('Folder', 'Utility');
 App::uses('File', 'Utility');
 
 class InitShell extends Shell {
-    
+
     public function main() {
         $Folder = new Folder(dirname(dirname(dirname(__FILE__))) . DS . "features");
-        $this->out("copy ".$Folder->pwd()." to Cake Root...");
-        $Folder->copy(array('to' => ROOT . DS . "features"));
+        $this->out("copy ".$Folder->pwd()." to Cake app/Test...");
+        $Folder->copy(array('to' => TESTS . DS . "features"));
 
         $File = new File(dirname(__FILE__) . DS . "behat.yml.default");
         $this->out("copy ".$File->name()." to App/Config...");
